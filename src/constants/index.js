d1aadd510e76e33e0e1a4116ff05570cc7f5315d
AhmadShahzad-0 <ajahmad678@gmail.com>
2025-07-18 22:47:37 +0500

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,9 +42,9 @@ const logoIconsList = [
   {
     imgPath: "/images/logos/company-logo-2.png",
   },
-  {
-    imgPath: "/images/logos/company-logo-3.png",
-  },
+  // {
+  //   imgPath: "/images/logos/company-logo-3.png",
+  // },
   {
     imgPath: "/images/logos/company-logo-4.png",
   },
@@ -54,21 +54,21 @@ const logoIconsList = [
   {
     imgPath: "/images/logos/company-logo-6.png",
   },
-  {
-    imgPath: "/images/logos/company-logo-7.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-8.png",
-  },
+  // {
+  //   imgPath: "/images/logos/company-logo-7.png",
+  // },
+  // {
+  //   imgPath: "/images/logos/company-logo-8.png",
+  // },
   {
     imgPath: "/images/logos/company-logo-9.png",
   },
-  {
-    imgPath: "/images/logos/company-logo-10.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-11.png",
-  },
+  // {
+  //   imgPath: "/images/logos/company-logo-10.png",
+  // },
+  // {
+  //   imgPath: "/images/logos/company-logo-11.png",
+  // },
 ];
 
 const abilities = [
